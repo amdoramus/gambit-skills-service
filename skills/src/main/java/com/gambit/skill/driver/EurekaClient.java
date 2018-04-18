53a6e7ccfffc3e2bb94e728ddfec8ab0705d67a9
Jordan Gibson- Price <jgibsonprice@gmail.com>
2018-04-18 01:03:36 -0400

    initial spring env setup

---
+++ skills/src/main/java/com/gambit/skill/driver/EurekaClient.java
@@ -0,0 +1,19 @@
+package com.gambit.skill.driver;
+
+import org.springframework.cloud.openfeign.FeignClient;
+
+import feign.RequestLine;
+
+
+/**
+ * PUT IN THE SERVICE WE ARE CONNECTING TO 
+ * @author Delano
+ *
+ */
+@FeignClient("")
+public interface EurekaClient {
+	
+	@RequestLine("GET")
+	public String getTestMessage();
+
+}
