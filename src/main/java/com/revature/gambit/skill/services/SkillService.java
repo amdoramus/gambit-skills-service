3099d6a91feabfd91b957ff19318420f9e16720e
Howard Chung <howardchung@temple.edu>
2018-04-20 15:21:47 -0400

    Removed find by id, to use find by name
    and organized order of methods to CRUD order

--- src/main/java/com/revature/gambit/skill/services/SkillService.java
+++ src/main/java/com/revature/gambit/skill/services/SkillService.java
@@ -24,45 +24,23 @@ public class SkillService implements ISkillService {
 	public Iterable<Skill> findAll() {
 		return this.skillRepository.findAll();
 	}
-
-	public Skill findById(int id) {
-		return this.skillRepository.findBySkillID(id);
-	}
-
-	@Override
-	public void deleteSkillViaId(int id) {
-
-		skillRepository.delete(findById(id));
-
-	}
-
-  @Autowired
-  private SkillRepository skillRepository;
-
-  public Skill create(Skill skill) { return this.skillRepository.save(skill); }
-
-  public Skill findByName(string name) { return this.skillRepository.findBySkillName(name); }
-    
-	@Transactional
-	public Skill saveSkill(Skill skill) {
-		return skillRepository.saveAndFlush(skill);
+	
+	public Iterable<Skill> findAllActive(){
+		return skillRepository.findAllByIsActive(true);
 	}
 	
 	public Skill findByName(String name) {
 		return skillRepository.findBySkillName(name);
 	}
-	
-	public List<Skill> findAllSkill(){
-		return skillRepository.findAll();
+
+	@Transactional
+	public Skill saveSkill(Skill skill) {
+		return skillRepository.saveAndFlush(skill);
 	}
-	
-	public List<Skill> findAllActive(){
-		return skillRepository.findAllByIsActive(true);
-	}
+
 	@Override
 	public void deleteSkillViaName(String name) {
 		skillRepository.delete(findByName(name)); //change to take a name instead of an int
-
 	}
 
 }
