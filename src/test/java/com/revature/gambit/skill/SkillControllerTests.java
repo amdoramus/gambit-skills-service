adcec37a1f9fe753bb5ecc7b72df97a44cffe118
jackktam <36857546+jackktam@users.noreply.github.com>
2018-04-27 11:45:15 -0400

    Changed pom and added documentation

--- src/test/java/com/revature/gambit/skill/SkillControllerTests.java
+++ src/test/java/com/revature/gambit/skill/SkillControllerTests.java
@@ -7,13 +7,13 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import com.revature.gambit.skill.controllers.SkillController;
-import com.revature.gambit.skill.services.SkillServiceImpl;
+import com.revature.gambit.skill.services.SkillService;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
@@ -21,6 +21,7 @@ import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest
 public class SkillControllerTests {
 
 	private MockMvc mvc;
@@ -29,7 +30,7 @@ public class SkillControllerTests {
 	private SkillController skillController;
 
 	@Mock
-	private SkillServiceImpl skillService;
+	private SkillService skillService;
 
 	@Before
 	public void setUp() {
@@ -117,7 +118,6 @@ public class SkillControllerTests {
 		
 		Gson gson =  new Gson();
 		String json = gson.toJson(skill1);
-		
 
 		mvc.perform(MockMvcRequestBuilders.put("/skill/{id}", 101)
 				.contentType(MediaType.APPLICATION_JSON).content(json)
